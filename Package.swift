ba34c5fd54b23c1c00f7bfa6e9407d7f80cf5f2f
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2025-11-26 15:17:03 +0300

    Добавляю Package.swift для представления репозитория в виде SPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "msspi",
+    platforms: [.macOS(.v12), .iOS(.v12)],
+    products: [.library(
+	name: "msspi_c",
+	type: .static,
+	targets: ["msspi_c"]
+    )],
+    targets: [
+	.target(
+	    name: "msspi_c",
+	    path: "src",
+	    publicHeadersPath: ".",
+	    cSettings: [
+		.headerSearchPath("../third_party/cprocsp/include")
+	    ]
+	)
+    ]
+)
